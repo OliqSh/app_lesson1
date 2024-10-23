2da18e2a287f1fa8aeaf6ab6e585b84aad14db59
OliqSh <olga263368@gmail.com>
2024-10-23 23:45:18 +0500

    Add files via upload

--- test_app.py
+++ test_app.py
@@ -1,1 +1,21 @@
+from flask import Flask, jsonify
+import unittest
 
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return jsonify({"Hello": "World"})
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_hello_world(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json(), {"Hello": "World"})
+
+if __name__ == "__main__":
+    unittest.main()
